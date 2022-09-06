5c891e36b6cb5c2c6bddabe1dff5670a44204caf
Alan Trieu <alantrieu@192-168-1-111.tpgi.com.au>
2022-09-06 23:28:52 +1000

    implement cycle traversal in results controller

--- Shut Eye/Controllers/ResultsViewController.swift
+++ Shut Eye/Controllers/ResultsViewController.swift
@@ -10,20 +10,47 @@ import UIKit
 class ResultsViewController: UIViewController {
     
     var time: [Int: String]?
+    var currentCycle = 3
 
     @IBOutlet weak var wakeImageView: UIImageView!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var incrementButton: UIButton!
+    @IBOutlet weak var decrementButton: UIButton!
     @IBOutlet weak var descriptionText: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         wakeImageView.image = UIImage(systemName: "sun.and.horizon")
-        timeLabel.text = time?[3]
-        descriptionText.text = "\(Double(90 * 3) / Double(60)) hours of sleep, or \(3) complete cycles"
+        timeLabel.text = time?[currentCycle]
+        descriptionText.text = "\(Double(90 * currentCycle) / Double(60)) hours of sleep, or \(currentCycle) complete cycles"
     }
 
+    @IBAction func incrementButtonPressed(_ sender: UIButton) {
+        // move to next cycle
+        if (currentCycle == 6) {
+            currentCycle = 3
+        } else {
+            currentCycle += 1
+        }
+        
+        timeLabel.text = time?[currentCycle]
+        descriptionText.text = "\(Double(90 * currentCycle) / Double(60)) hours of sleep, or \(currentCycle) complete cycles"
+    }
+    
+    @IBAction func decrementButtonPressed(_ sender: UIButton) {
+        // move to previous cycle
+        if (currentCycle == 3) {
+            currentCycle = 6
+        } else {
+            currentCycle -= 1
+        }
+        
+        timeLabel.text = time?[currentCycle]
+        descriptionText.text = "\(Double(90 * currentCycle) / Double(60)) hours of sleep, or \(currentCycle) complete cycles"
+    }
+    
+    
     @IBAction func recalculatePressed(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
