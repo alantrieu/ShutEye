767bc123fb816479ad8f5baf376c087d7e86492a
Alan Trieu <alantrieu@192-168-1-111.tpgi.com.au>
2022-09-07 20:29:42 +1000

    add new controller to calculate when to sleep

--- Shut Eye/Controllers/WakeController.swift
+++ Shut Eye/Controllers/WakeController.swift
@@ -5,4 +5,42 @@
 //  Created by Alan Trieu on 7/9/2022.
 //
 
-import Foundation
+import UIKit
+
+class WakeController: UIViewController {
+    
+    let formatter = DateFormatter()
+    var timeString: String?
+    let cycleCalc = CycleCalculator()
+    
+    @IBOutlet weak var logoView: UIImageView!
+    @IBOutlet weak var timePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        formatter.dateFormat = "hh:mm a"
+        
+        logoView.image = UIImage(systemName: K.Symbols.sun)
+        timePicker.setValue(K.Colors.textColor, forKey: "textColor")
+        timeString = formatter.string(from: timePicker.date)
+    }
+    
+    @IBAction func timeChanged(_ sender: UIDatePicker) {
+        timeString = formatter.string(from: sender.date)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        performSegue(withIdentifier: K.Segues.results, sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == K.Segues.results) {
+            let cycleData = CycleData(cycleTimes: cycleCalc.calculateCycles(time: timePicker.date, with: formatter, sleep: false))
+            
+            let destVC = segue.destination as! ResultsViewController
+            destVC.cycleData = cycleData
+            destVC.titleText = K.Segues.wakeResults
+        }
+    }
+}
