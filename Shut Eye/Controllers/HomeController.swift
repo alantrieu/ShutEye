d58105057d9b249f86da129adf6d9f593cf610df
Alan Trieu <alantrieu@192-168-1-111.tpgi.com.au>
2022-09-07 20:31:13 +1000

    add home controller with nav function

--- Shut Eye/Controllers/HomeController.swift
+++ Shut Eye/Controllers/HomeController.swift
@@ -5,4 +5,16 @@
 //  Created by Alan Trieu on 7/9/2022.
 //
 
-import Foundation
+import UIKit
+
+class HomeController: UIViewController {
+    @IBOutlet weak var appName: UILabel!
+    @IBOutlet weak var appLogo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        appName.text = K.appName
+        appLogo.image = UIImage(systemName: "zzz")
+    }
+}
